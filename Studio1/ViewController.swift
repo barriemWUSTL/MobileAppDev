5ea9524f05823b62c12558d39c5c93854a40a2d3
barriem <barriem@wustl.edu>
2019-09-01 16:31:15 -0500

    Initial Commit

---
+++ Studio1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Studio1
+//
+//  Created by Mustapha Barrie on 9/1/19.
+//  Copyright © 2019 Mustapha Barrie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Studio1Tests/Studio1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Studio1Tests.swift
+//  Studio1Tests
+//
+//  Created by Mustapha Barrie on 9/1/19.
+//  Copyright © 2019 Mustapha Barrie. All rights reserved.
+//
+
+import XCTest
+@testable import Studio1
+
+class Studio1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
